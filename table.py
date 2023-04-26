bf9792a41388940aea405d63deeb5166fb77efa0
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-26 15:21:32 +0530

    Update table.py

--- table.py
+++ table.py
@@ -7,7 +7,7 @@ from google.oauth2 import service_account
 PROJECT_ID = 'clean-phoenix-378215'
 DATASET_ID = 'livestock'
 TABLE_ID = 'first_table'
-credentials = service_account.Credentials.from_service_account_file(os.getenv('creds'))
+credentials = service_account.Credentials.from_service_account_file(os.getenv('CREDS'))
 
 # Set up the BigQuery client
 client = bigquery.Client(credentials=credentials, project=PROJECT_ID)
